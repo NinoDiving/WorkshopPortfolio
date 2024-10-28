cc1cbc5b732fcd2c1579fff343b1e34def0386a3
Chiche Nicolas <chiche.nicolas@gmail.com>
2024-10-28 21:45:04 +0100

    Form full props

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,71 @@
 import Citation from "./components/citation/citation";
 import NavBar from "./components/competence/NavBar/NavBar";
 import Competence from "./components/competence/competence";
+import Form from "./components/Form/form";
 
 function App() {
-	const navigation = [
-		{
-			text: "Accueil",
-			href: "#home",
-		},
-		{
-			text: "A propos",
-			href: "About",
-		},
-		{
-			text: "Compétences",
-			href: "Compétences",
-		},
-		{
-			text: "Profil",
-			href: "Profil",
-		},
-		{
-			text: "Contact",
-			href: "Contact",
-		},
-	];
-	return (
-		<>
-			<NavBar logo="src/assets/logo-1.png" links={navigation} />
-			<Competence />
-			<Citation />
-		</>
-	);
+  const navigation = [
+    {
+      text: "Accueil",
+      href: "#home",
+    },
+    {
+      text: "A propos",
+      href: "About",
+    },
+    {
+      text: "Compétences",
+      href: "Compétences",
+    },
+    {
+      text: "Profil",
+      href: "Profil",
+    },
+    {
+      text: "Contact",
+      href: "Contact",
+    },
+  ];
+  const formFields = [
+    {
+      label: "Nom",
+      name: "name",
+      type: "text",
+      placeholder: "Entrez votre nom",
+    },
+    {
+      label: "Email",
+      name: "email",
+      type: "email",
+      placeholder: "Entrez votre email",
+    },
+    {
+      label: "Téléphone",
+      name: "phone",
+      type: "tel",
+      placeholder: "Entrez votre numéro de téléphone",
+    },
+    {
+      label: "Objet",
+      name: "objet",
+      type: "objet",
+      placeholder: "Objet",
+    },
+    {
+      label: "Message",
+      name: "message",
+      type: "text",
+      placeholder: "Entrez votre message",
+    },
+  ];
+  return (
+    <>
+      <NavBar logo="./src/assets/stephPic.jpg" links={navigation} />
+      <Competence />
+      <Citation />
+      <Form fields={formFields} />
+    </>
+  );
 }
 
 export default App;
