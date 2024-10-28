fabfe1c743ce1ab436a6fbe53e0a45b3789c82d2
Nino Jautee <jauteenino@gmail.com>
2024-10-28 18:32:16 +0100

    add Manon and Nico to main

--- src/components/competence/competence.tsx
+++ src/components/competence/competence.tsx
@@ -9,27 +9,66 @@ export default function Competence() {
 				fait que l’on en apprend tous les jours, d’autant plus en étant
 				impliqué, curieux et passionné.
 			</p>
-			<section className="skillsProgress">
-				<label htmlFor="php">PHP</label>
-				<progress id="php" max="100" value="82">
-					82%%
-				</progress>
-				<label htmlFor="sql">SQL</label>
-				<progress id="sql" max="100" value="80">
-					80%
-				</progress>
-				<label htmlFor="symfony">SYMFONY</label>
-				<progress id="php" max="100" value="63">
-					63%
-				</progress>
-				<label htmlFor="smarty">SMARTY</label>
-				<progress id="php" max="100" value="60">
-					60%
-				</progress>
-			</section>
-			<figure className="imgSkills">
-				<img src="./src/assets/competence.png" alt="" />
-				<div>
+			<section className="imgSkills">
+				<article className="skillsProgress">
+					<h4>Back-end</h4>
+					<label htmlFor="php">PHP</label>
+					<progress id="php" max="100" value="82">
+						82%%
+					</progress>
+					<label htmlFor="sql">SQL</label>
+					<progress id="sql" max="100" value="80">
+						80%
+					</progress>
+					<label htmlFor="symfony">SYMFONY</label>
+					<progress id="php" max="100" value="63">
+						63%
+					</progress>
+					<label htmlFor="">CODEIGNITER</label>
+					<progress id="php" max="100" value="60">
+						60%
+					</progress>
+					<label htmlFor="smarty">SMARTY</label>
+					<progress id="php" max="100" value="60">
+						60%
+					</progress>
+					<h4>Front-end</h4>
+					<label htmlFor="smarty">SMARTY</label>
+					<progress id="php" max="100" value="60">
+						60%
+					</progress>
+					<label htmlFor="smarty">SMARTY</label>
+					<progress id="php" max="100" value="60">
+						60%
+					</progress>
+					<label htmlFor="smarty">SMARTY</label>
+					<progress id="php" max="100" value="60">
+						60%
+					</progress>
+					<h4>CMS</h4>
+					<label htmlFor="smarty">SMARTY</label>
+					<progress id="php" max="100" value="60">
+						60%
+					</progress>
+					<label htmlFor="smarty">SMARTY</label>
+					<progress id="php" max="100" value="60">
+						60%
+					</progress>
+					<label htmlFor="smarty">SMARTY</label>
+					<progress id="php" max="100" value="60">
+						60%
+					</progress>
+					<h4>Divers</h4>
+					<label htmlFor="smarty">SMARTY</label>
+					<progress id="php" max="100" value="60">
+						60%
+					</progress>
+					<label htmlFor="smarty">SMARTY</label>
+					<progress id="php" max="100" value="60">
+						60%
+					</progress>
+				</article>
+				<figure>
 					<img className="stephProfil" src="./src/assets/stephPic.jpg" alt="" />
 					<a
 						className="downloadPic"
@@ -38,8 +77,8 @@ export default function Competence() {
 					>
 						Télécharger ma photo
 					</a>
-				</div>
-			</figure>
+				</figure>
+			</section>
 		</section>
 	);
 }
