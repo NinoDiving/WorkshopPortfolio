d6a4245a0bbb70d5ba7fee9a34ad07c71bd52938
nicochnz <chiche.nicolas@gmail.com>
2024-10-29 10:21:12 +0100

    navbar

--- src/components/competence/NavBar/NavBar.tsx
+++ src/components/competence/NavBar/NavBar.tsx
@@ -2,36 +2,31 @@ import { useState } from "react";
 import "./NavBar.css";
 
 interface NavLink {
-  text: string;
-  href: string;
+	text: string;
+	href: string;
 }
 interface NavBarProps {
-  logo: string;
-  links: NavLink[];
+	logo: string;
+	links: NavLink[];
 }
 
 export default function NavBar({ logo, links }: NavBarProps) {
-  return (
-    <header>
-      <nav className="NavBar">
-        <img className="logo" src={logo} alt="logo" />
-        <ul className="navLinks">
-          {links.map((link, index) => (
-            <li key={index} className="NavItem">
-              <a href={link.href}>{link.text}</a>
-            </li>
-          ))}
-        </ul>
-      </nav>
-      <h2 className="A_Propos">Stéphanie Awkward</h2>
-      <p className="A_Propos">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae, eveniet
-        eaque ducimus voluptas ullam in consectetur minima neque, quaerat enim
-        alias, ad dolorem.
-      </p>
-      <button className="firstButton" type="button">
-        Qui suis-je ?
-      </button>
-    </header>
-  );
+	return (
+		<header>
+			<nav className="NavBar">
+				<img className="logo" src={logo} alt="logo" />
+				<ul className="navLinks">
+					{links.map((link, index) => (
+						<li key={index} className="NavItem">
+							<a href={link.href}>{link.text}</a>
+						</li>
+					))}
+				</ul>
+			</nav>
+			<h2 className="title">Stéphanie Awkward</h2>
+			<button className="firstButton" type="button">
+				Qui suis-je ?
+			</button>
+		</header>
+	);
 }
